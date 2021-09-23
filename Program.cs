e3bea30cf698df6e63331cc79bb2b1393dc26f87
varnikak00 <varnikak7070@gmail.com>
2021-09-24 01:10:22 +0530

    finding vowes

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace VowelOrConsonant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.Write("\n\n");
+            Console.Write("check whether the input alphabet is a vowel or not:\n");
+            Console.Write("-----------------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input an Alphabet (A-Z or a-z) : ");
+            ch = Convert.ToChar(Console.ReadLine().ToLower());
+            int i = ch;
+            if (i >= 48 && i <= 57)
+            {
+                Console.Write("You entered a number, Please enter an alpahbet.");
+            }
+            else
+            {
+                switch (ch)
+                {
+                    case 'a':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'i':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'o':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'u':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'e':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    default:
+                        Console.WriteLine("The Alphabet is not a vowel");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
