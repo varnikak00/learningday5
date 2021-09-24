e851602be2fbd8ecd14f2c4a8437387e0ecf2ffe
varnikak00 <91057679+varnikak00@users.noreply.github.com>
2021-09-24 10:15:11 +0530

    Add files via upload

---
+++ power of two/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LargestAmongThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.Write("\n\n");
+            Console.Write("Find the largest of three numbers:\n");
+            Console.Write("----");
+            Console.Write("\n\n");
+
+            Console.Write("Input the 1st number :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the  2nd number :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the 3rd  number :");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello World!");
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+
+                    {
+                        Console.Write("The 1st Number is the greatest among three. \n\n");
+
+
+            else
+                        {
+                            Console.Write("The 3rd Number is the greatest among three. \n\n");
+
+
+                else if (num2 > num3)
+                                Console.Write("The 2nd Number is the greatest among three \n\n");
+                            else
+                                Console.Write("The 3rd Number is the greatest among three \n\n");
+                        }
+
+
+                    }
+                }
+    
